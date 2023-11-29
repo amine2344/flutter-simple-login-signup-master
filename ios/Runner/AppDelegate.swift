5a7ad6aa3167340c3e4db40cdf384d0a83793a03
amine2344 <118748281+amine2344@users.noreply.github.com>
2023-11-29 15:01:52 +0000

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,17 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    let flutterViewController: FlutterViewController = window?.rootViewController as! FlutterViewController
+
+    let channel = FlutterMethodChannel(name: "mychannel", binaryMessenger: flutterViewController.binaryMessenger)
+
+    // Register your plugins here
+    let flutterEngine = flutterViewController.engine
+
+    // Example registration for a camera plugin
+    let cameraPlugin = CameraPlugin()
+    cameraPlugin.register(with: flutterEngine)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
