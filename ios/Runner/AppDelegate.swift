7a529854943a68204a9542750139438308fe2628
amine2344 <118748281+amine2344@users.noreply.github.com>
2023-11-29 23:24:33 +0000

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,6 @@
 import UIKit
 import Flutter
 import FirebaseCore
-import FirebaseMessaging
 import UserNotifications
 
 @UIApplicationMain
@@ -23,14 +22,8 @@ import UserNotifications
         
         application.registerForRemoteNotifications()
         
-        // Configure Firebase
+        // Configure Firebase (if needed)
         FirebaseApp.configure()
-        
-        // Set FCM token refresh delegate
-        Messaging.messaging().delegate = self
-        
-        // Add observer for handling FCM token refresh
-        NotificationCenter.default.addObserver(self, selector: #selector(self.tokenRefreshNotification(_:)), name: NSNotification.Name.MessagingRegistrationTokenRefreshed, object: nil)
         
         // Initialize Flutter plugins
         GeneratedPluginRegistrant.register(with: self)
@@ -38,33 +31,15 @@ import UserNotifications
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
     
-    override func applicationDidBecomeActive(_ application: UIApplication) {
-        // Connect to FCM for token refresh
-        connectToFcm()
+    // Handle the device token for push notifications
+    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        print("Device Token: \(token)")
+        // Send the device token to your server if needed
     }
     
-    // Handle FCM token refresh
-    @objc func tokenRefreshNotification(_ notification: NSNotification) {
-        connectToFcm()
-    }
-    
-    // Connect to FCM
-    func connectToFcm() {
-        // Disconnect if connected
-        Messaging.messaging().shouldEstablishDirectChannel = true
-    }
-}
-
-// Implement FCM delegate methods
-extension AppDelegate: MessagingDelegate {
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        if let fcmToken = fcmToken {
-            print("FCM Token: \(fcmToken)")
-            // You can send the FCM token to your server for targeted push notifications
-        }
-    }
-    
-    func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
-        print("Received data message: \(remoteMessage.appData)")
+    // Handle notification settings
+    override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register for remote notifications with error: \(error.localizedDescription)")
     }
 }
