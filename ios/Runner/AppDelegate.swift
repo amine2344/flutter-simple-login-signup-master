b45ca0935ee40c942aa7a9dcc963524b3b0ec1bf
amine2344 <118748281+amine2344@users.noreply.github.com>
2023-11-29 16:35:30 +0000

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,15 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let flutterViewController: FlutterViewController = window?.rootViewController as! FlutterViewController
+        GeneratedPluginRegistrant.register(with: self)
+
+        // Your additional setup code...
+
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
