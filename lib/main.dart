b8e797391085e4cbcce74758f989f36c132cd9f2
amine2344 <118748281+amine2344@users.noreply.github.com>
2023-11-30 11:52:03 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -34,86 +34,11 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
 
 Future main() async {
-  WidgetsFlutterBinding.ensureInitialized(); 
+   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options:  DefaultFirebaseOptions.currentPlatform,
-
-
+    options: DefaultFirebaseOptions.currentPlatform,
   );
-   // TODO: Request permission
-    final messaging = FirebaseMessaging.instance;
-
-final settings = await messaging.requestPermission(
- alert: true,
- announcement: false,
- badge: true,
- carPlay: false,
- criticalAlert: false,
- provisional: false,
- sound: true,
-);
-
- if (kDebugMode) {
-   print('Permission granted: ${settings.authorizationStatus}');
- }
-
- FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-    await initializeOtherClass();
-
-// TODO: replace with your own VAPID key
- const vapidKey = "BDF8fXiJSDbqRhbvSSKo1dt2NmmTWr1RkVX2Hkl8pdBl_Svfy55x5ZLrObzZq0wTasTgcmySv3Ro7WBtsrB0kmM";
-// use the registration token to send messages to users from your trusted server environment
- String? token;
-
- /* if (DefaultFirebaseOptions.currentPlatform == DefaultFirebaseOptions.web) {
-   token = await messaging.getToken(
-     vapidKey: vapidKey,
-   );
- } else {
-   token = await messaging.getToken();
- }
-
- if (kDebugMode) {
-   print('Registration Token=$token');
- }
- */
-   
- // TODO: Register with FCM
-
-
- // TODO: Set up foreground message handler
- FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-   if (kDebugMode) {
-     print('Handling a foreground message: ${message.messageId}');
-     print('Message data: ${message.data}');
-     print('Message notification: ${message.notification?.title}');
-     print('Message notification: ${message.notification?.body}');
-   }
-
-   _messageStreamController.sink.add(message);
- });
-
- // TODO: Set up background message handler
-  // Configure Firebase Cloud Messaging
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  FirebaseMessaging messagings = FirebaseMessaging.instance;
-
-  // Request permission for displaying notifications
-  await messagings.requestPermission();
-  
-  // Obtain the FCM token
-
-  // Register a callback for receiving messages
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    // Handle the incoming message
-  });
-
-  // Handle any initial notification after the app launch
-  RemoteMessage? initialMessage = await messaging.getInitialMessage();
-  if (initialMessage != null) {
-    // Handle the initial message
-  }
-
+  await initializeOtherClass();
   runApp(const MyApp());
 
 
@@ -175,7 +100,7 @@ class _MyAppWithSplashState extends State<MyAppWithSplash> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: main(),
+      future: initializeOtherClass(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return const AuthPage(); // Navigate to AuthPage after initialization
