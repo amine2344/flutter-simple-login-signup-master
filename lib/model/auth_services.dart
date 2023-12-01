4b3270a3b5f60c5d21c30637d7e41a063ea7b816
amine2344 <118748281+amine2344@users.noreply.github.com>
2023-12-01 18:14:26 +0000

    Update auth_services.dart

--- lib/model/auth_services.dart
+++ lib/model/auth_services.dart
@@ -1,25 +1,15 @@
 
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
 
-  // firebase messagings : 
-  final firebasemessaging = FirebaseMessaging.instance ;
-  Future<void> initNotification () async { 
-    // request permissions from user : 
-    await firebasemessaging.requestPermission();
-    //fetch FCM tokent from device 
-    final FCMtoken = await firebasemessaging.getToken() ; 
 
-    print ("token : $FCMtoken");
-  }
-  void handleMessages(RemoteMessage? message){ 
-    if(message == null ){
-      return ; 
-    }
+
+  Future<void> initNotification () async { 
+
    /*  NavigatorKey.currentState?.pushNamed{
       'EdashedBoardScreen', 
       arguments: message , 
@@ -29,10 +19,8 @@ class AuthService {
   }
   Future inithandleNotifications() async { 
     //handle notifications if the app terminated and  now opened
-    FirebaseMessaging.instance.getInitialMessage().then(handleMessages);
-    //attach event listenears when a notification open the app 
-    FirebaseMessaging.onMessageOpenedApp.listen(handleMessages);
-  }
+
+ }
   //google sign in :  
   signInWithGoogle() async {
     final GoogleSignInAccount? guser  = await GoogleSignIn().signIn() ; 
