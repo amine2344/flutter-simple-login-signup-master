198f67ae44e9490df550e9ab05350765c45a62bb
amine2344 <118748281+amine2344@users.noreply.github.com>
2023-12-02 10:59:38 +0000

    Update auth_page.dart

--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:loginSignup/screens/ea_dashed_board_screen.dart';
-import 'package:loginSignup/screens/login_or_register.dart';
+import 'package:loginSignup/screens/EADashedBoardScreen.dart';
+import 'package:loginSignup/screens/LoginOrRegister.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({Key? key});
