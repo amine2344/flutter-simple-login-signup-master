ed5bd38c67b5c434905b6b3c57214958de37ad01
amine2344 <118748281+amine2344@users.noreply.github.com>
2023-12-02 10:54:24 +0000

    Update auth_page.dart

--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -1,25 +1,35 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:loginSignup/screens/EADashedBoardScreen.dart';
-import 'package:loginSignup/screens/LoginOrRegister.dart';
+import 'package:loginSignup/screens/ea_dashed_board_screen.dart';
+import 'package:loginSignup/screens/login_or_register.dart';
 
-class AuthPage extends StatelessWidget{ 
-  const AuthPage({super.key}); 
-  @override 
-  Widget build(BuildContext context){
+class AuthPage extends StatelessWidget {
+  const AuthPage({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
+      body: FutureBuilder(
+        // Use FutureBuilder to wait for Firebase.initializeApp()
+        future: Firebase.initializeApp(),
         builder: (context, snapshot) {
-          if(snapshot.hasData){
-            return const EADashedBoardScreen();
-          }else {
-            return const LoginOrRegister();
+          if (snapshot.connectionState == ConnectionState.done) {
+            return StreamBuilder(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return const EADashedBoardScreen();
+                } else {
+                  return const LoginOrRegister();
+                }
+              },
+            );
+          } else {
+            // Show a loading indicator while waiting for Firebase.initializeApp()
+            return CircularProgressIndicator();
           }
-        }
-        
-        )
-
+        },
+      ),
     );
   }
 }
