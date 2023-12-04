85d5e60e0a339aff6f17e336bce094e2e1c510e8
amine2344 <118748281+amine2344@users.noreply.github.com>
2023-12-04 10:56:06 +0000

    Update auth_page.dart

--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -3,33 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:loginSignup/screens/EADashedBoardScreen.dart';
 import 'package:loginSignup/screens/LoginOrRegister.dart';
 import 'package:firebase_core/firebase_core.dart';
-class AuthPage extends StatelessWidget {
-  const AuthPage({Key? key});
 
-  @override
-  Widget build(BuildContext context) {
+class AuthPage extends StatelessWidget{ 
+  const AuthPage({super.key}); 
+  @override 
+  Widget build(BuildContext context){
     return Scaffold(
-      body: FutureBuilder(
-        // Use FutureBuilder to wait for Firebase.initializeApp()
-        future: Firebase.initializeApp(),
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return StreamBuilder(
-              stream: FirebaseAuth.instance.authStateChanges(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return const EADashedBoardScreen();
-                } else {
-                  return const LoginOrRegister();
-                }
-              },
-            );
-          } else {
-            // Show a loading indicator while waiting for Firebase.initializeApp()
-            return CircularProgressIndicator();
+          if(snapshot.hasData){
+            return const EADashedBoardScreen();
+          }else {
+            return const LoginOrRegister();
           }
-        },
-      ),
+        }
+        
+        )
+
     );
   }
 }
